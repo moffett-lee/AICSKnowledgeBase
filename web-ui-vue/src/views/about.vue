a8f37ab930596e9c0ba0c49c88733ae88b96f385
“ext.liyuze” <ext.liyuze@jd.com>
2022-01-07 16:15:30 +0800

    v1-vue3 实战

--- web-ui-vue/src/views/about.vue
+++ web-ui-vue/src/views/about.vue
@@ -2,39 +2,39 @@
   <a-layout>
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
       <div class="about">
-        <h2>我是甲蛙，13年Java全栈，慕课网讲师</h2>
-        <div><b>专注Java全栈技术分享，汇总13年实战经验</b></div>
+        <h2>我是Amber·L，偏Java后端的全栈工程师。</h2>
+        <div><b>这是一个为自己平时学习所开发的知识库笔记系统，简单至上。</b></div>
         <div>
-          <b>项目实战：
-            <a href="https://coding.imooc.com/class/474.html" target="_blank">
-            《SpringBoot知识体系+Vue3全家桶 前后端分离 实战WIKI知识库系统》
+          <b>GitHub：
+            <a href="https://github.com/Ezuy-Lee" target="_blank">
+             平时学习和研发的一些开源项目。
             </a>
           </b>
         </div>
         <div>
-          <b>项目实战：
+          <b>昆虫云社区：
             <a href="https://coding.imooc.com/class/416.html" target="_blank">
-            《Spring Cloud + Vue 前后端分离 开发企业级在线视频课程系统》
+            专注于开发者技术交流的社区中心。
             </a>
           </b>
         </div>
         <div>
-          <b>免费课程：
+          <b>昆虫云官网：
             <a href="https://www.imooc.com/learn/1160" target="_blank">
-            《开发工具IDEA从入门到爱不释手》
+             Sass多租户低代码平台。
             </a>
           </b>
         </div>
         <br>
-        <div><b>QQ：82144921，<span style="color: rgb(194, 79, 74)">阿里云产品使用咨询、购买优惠</span>，加QQ，备注：阿里云</b></div>
-        <div><b>微信号：jiawa1986</b></div>
-        <div><b>QQ群：174545069，Java全栈交流群</b></div>
+        <div><b>QQ：619147075</b></div>
+        <div><b>微信号：Rainze-Lee</b></div>
+        <div><b>QQ群：619147075</b></div>
         <div><b>个人全栈技术博客：
-          <a href="http://www.jiawablog.com" target="_blank">甲蛙博客</a></b>
+          <a href="https://blog.liyuze.work/" target="_blank">X-Bugs</a></b>
         </div>
-        <div style="color: rgb(194, 79, 74)"><b>公众号：甲蛙全栈，扫码关注</b></div>
+        <div style="color: rgb(194, 79, 74)"><b>公众号：LOVECODE，扫码关注</b></div>
         <div>
-          <img src="/image/jiawaquanzhan.jpg">
+          <img src="/image/weix-wl.png">
         </div>
       </div>
     </a-layout-content>
