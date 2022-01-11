3a4a826fd6949d44b7690ed629473b2b2c4a0d83
“ext.liyuze” <ext.liyuze@jd.com>
2022-01-11 19:58:56 +0800

    新增活跃度vue

--- web-ui-vue/src/views/tag.vue
+++ web-ui-vue/src/views/tag.vue
@@ -104,6 +104,78 @@
     </a-layout>
 </template>
 
+<script lang="ts">
+    import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+    import axios from 'axios';
+    import { message } from 'ant-design-vue';
+    import {Tool} from "@/util/tool";
+
+
+    export default defineComponent({
+        name: 'Tag',
+        components: {
+
+        },
+        setup() {
+            const ebooks = ref();
+
+            const openKeys =  ref();
+
+            const level1 =  ref();
+            let categorys: any;
+            /**
+             * 查询所有标签
+             **/
+            const handleQueryCategory = () => {
+                axios.get("/category/getCategoryList").then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        categorys = data.data;
+                        console.log("原始数组：", categorys);
+
+                        // 加载完分类后，将侧边栏全部展开
+                        openKeys.value = [];
+                        for (let i = 0; i < categorys.length; i++) {
+                            openKeys.value.push(categorys[i].id)
+                        }
+
+                        level1.value = [];
+                        level1.value = Tool.array2Tree(categorys, 0);
+                        console.log("树形结构：", level1.value);
+                    } else {
+                        message.error(data.msg);
+                    }
+                });
+            };
+
+            onMounted(() => {
+                handleQueryCategory();
+                // handleQueryEbook();
+            });
+
+            return {
+                ebooks,
+                // ebooks2: toRef(ebooks1, "books"),
+                // listData,
+                pagination: {
+                    onChange: (page: any) => {
+                        console.log(page);
+                    },
+                    pageSize: 3,
+                },
+                // actions: [
+                //   { type: 'StarOutlined', text: '156' },
+                //   { type: 'LikeOutlined', text: '156' },
+                //   { type: 'MessageOutlined', text: '2' },
+                // ],
+
+                level1,
+
+                openKeys
+            }
+        }
+    });
+</script>
 <style>
 .tag-main {
    line-height: 6vh;
