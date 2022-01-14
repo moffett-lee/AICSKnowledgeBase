be20063e369bd170763b4dbed8b47542b2ae61ef
Amber-L <ezuylee@gmail.com>
2022-01-14 23:08:54 +0800

    amber-api

--- web-ui-vue/src/main.ts
+++ web-ui-vue/src/main.ts
@@ -8,7 +8,6 @@ import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
 import {Tool} from "@/util/tool";
 import { message } from 'ant-design-vue';
-import * as G2 from '@antv/g2';
 
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
@@ -46,7 +45,6 @@ axios.interceptors.response.use(function (response) {
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
-app.config.globalProperties.$G2 = G2;
 // 全局使用图标
 const icons: any = Icons;
 for (const i in icons) {
