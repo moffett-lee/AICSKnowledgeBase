e566fe6ec96b3abaaa60f9090c703ed9e9526dbd
“ext.liyuze” <ext.liyuze@jd.com>
2022-01-20 20:10:02 +0800

    文章数量

--- src/main/java/com/amber/insect/knowledgebase/service/impl/EbookSnapshotServiceImpl.java
+++ src/main/java/com/amber/insect/knowledgebase/service/impl/EbookSnapshotServiceImpl.java
@@ -1,30 +1,16 @@
 package com.amber.insect.knowledgebase.service.impl;
 
 import com.alibaba.fastjson.JSON;
-import com.amber.insect.knowledgebase.common.StatisticResp;
 import com.amber.insect.knowledgebase.dto.EbookSnapshotDto;
-import com.amber.insect.knowledgebase.entity.EbookSnapshotEntity;
-import com.amber.insect.knowledgebase.entity.UserEntity;
-import com.amber.insect.knowledgebase.enums.CommonConstants;
+import com.amber.insect.knowledgebase.repository.ContentRepository;
 import com.amber.insect.knowledgebase.repository.EbookSnapshotRepository;
 import com.amber.insect.knowledgebase.service.IEbookSnapshotService;
-import com.amber.insect.knowledgebase.util.CopyUtil;
-import org.springframework.beans.BeanUtils;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -34,6 +20,9 @@ public class EbookSnapshotServiceImpl implements IEbookSnapshotService {
     @Resource
     private EbookSnapshotRepository ebookSnapshotRepository;
 
+    @Resource
+    private ContentRepository contentRepository;
+
     @Override
     public List<EbookSnapshotDto> getStatistic() {
        List<Map<String,Object>> mapList = ebookSnapshotRepository.getStatistic();
@@ -41,7 +30,9 @@ public class EbookSnapshotServiceImpl implements IEbookSnapshotService {
         if (!CollectionUtils.isEmpty(mapList)) {
             mapList.forEach(map -> {
                 EbookSnapshotDto o = JSON.parseObject(JSON.toJSONString(map), EbookSnapshotDto.class);
-                    dtos.add(o);
+                long count = contentRepository.count();
+                o.setArticleCount(count);
+                dtos.add(o);
             });
        }
         return dtos;
